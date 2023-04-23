4dce785b9c8f9a234f02f2caffab088da3212c7f
Жора <zhorabologov@gmal.com>
2023-04-23 12:22:51 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,15 @@ public class Main {
     public static void main(String[] args) {
 
         employees[0] = new Employee ("Петров Петр Петрович" , 1 , 10000);
-        employees[1] = new Employee ("Иванов Иван Иванович" , 1 , 10000);
-        employees[2] = new Employee ("Сидоров Сидор Сидорович" , 1 , 10000);
-        employees[3] = new Employee ("Николаев Николай Николаевич" , 1 , 10000);
-        employees[4] = new Employee ("Пупкин Степан Аркадьевич" , 1 , 10000);
-        employees[5] = new Employee ("Евдокимов Евдоким Евдокимович" , 1, 10000);
-        employees[6] = new Employee ("Захаров Захар Закорович" , 1 , 10000);
-        employees[7] = new Employee ("Никитин Никита Никитович" , 1 , 10000);
-        employees[8] = new Employee ("Кривошеев Сергей Анатольевич" , 1 , 10000 );
-        employees[9] = new Employee ("Петровский Александр Владимирович" , 1 , 10000);
+        employees[1] = new Employee ("Иванов Иван Иванович" , 3 , 20000);
+        employees[2] = new Employee ("Сидоров Сидор Сидорович" , 4 , 30000);
+        employees[3] = new Employee ("Николаев Николай Николаевич" , 5 , 40000);
+        employees[4] = new Employee ("Пупкин Степан Аркадьевич" , 2 , 50000);
+        employees[5] = new Employee ("Евдокимов Евдоким Евдокимович" , 3, 60000);
+        employees[6] = new Employee ("Захаров Захар Закорович" , 4 , 70000);
+        employees[7] = new Employee ("Никитин Никита Никитович" , 1 , 80000);
+        employees[8] = new Employee ("Кривошеев Сергей Анатольевич" , 2 , 90000 );
+        employees[9] = new Employee ("Петровский Александр Владимирович" , 2 , 220000);
 
 
         for (Employee i : employees){
@@ -80,7 +80,7 @@ public class Main {
     }
 
     private static Employee getEmployeeWithMaxSalary(Employee[] employees) {
-        Employee result = employees[0];
+        Employee result = employees[9];
 
         for (Employee employee : employees) {
             if (employee.getSalary () > result.getSalary ()) {
